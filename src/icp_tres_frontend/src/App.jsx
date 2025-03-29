42fca4450f7157ecfd6cbccdb94aa0d75ba37a3c
Esvan2076 <estividivi2016@hotmail.com>
2025-03-29 11:01:36 -0600

    Crea el proyecto

--- src/icp_tres_frontend/src/App.jsx
+++ src/icp_tres_frontend/src/App.jsx
@@ -15,15 +15,9 @@ function App() {
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </main>
   );
 }
